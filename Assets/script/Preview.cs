ac44cfc9d5a63658ee5dc63caeadfb556ebabedc
fanhl <dyguests@qq.com>
2024-03-12 23:44:42 +0800

    save preview.

--- Assets/script/Preview.cs
+++ Assets/script/Preview.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace script
 {
     public class Preview : MonoBehaviour
     {
+        public Text previewText;
+
         // private TetrominoData mData;
 
         public void Init(TetrominoData data)
         {
             // mData = data;
+            this.previewText.text = data.tetromino.ToString();
         }
     }
 }
